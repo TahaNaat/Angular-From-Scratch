ab2a09542a48a4e5b2c75b7284a3741194143ff3
TahaNaat <naat.taha@esprit.tn>
2024-11-23 12:38:46 +0100

    added new components + finished TD1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResidencesComponent } from './components/residences/residences.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home', component: ResidencesComponent},
+  {path : '', redirectTo: '/home', pathMatch: 'full'},
+  {path : '**', component: NotFoundComponent}
+  
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { ResidencesComponent } from './components/residences/residences.component';
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ResidenceDetailsComponentComponent } from './components/residences/residence-details-component/residence-details-component.component';
+import { AddResidenceComponentComponent } from './components/residences/add-residence-component/add-residence-component.component';
+import { ApartmentsComponentComponent } from './components/Appartments/apartments-component/apartments-component.component';
+import { ApartmentsByResidenceComponentComponent } from './components/Appartments/apartments-by-residence-component/apartments-by-residence-component.component';
+import { AddApartmentComponentComponent } from './components/Appartments/add-apartment-component/add-apartment-component.component';
 
 @NgModule({
   declarations: [
@@ -12,11 +19,18 @@ import { ResidencesComponent } from './components/residences/residences.componen
     AppComponent,
     HeaderComponent,
     ResidencesComponent,
+    NotFoundComponent,
+    ResidenceDetailsComponentComponent,
+    AddResidenceComponentComponent,
+    ApartmentsComponentComponent,
+    ApartmentsByResidenceComponentComponent,
+    AddApartmentComponentComponent
   ],
   imports: [
     //importer des modules externs
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent], //c pour declarer le composant racine
